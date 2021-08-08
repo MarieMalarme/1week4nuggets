4ee6bf03e4f0fef17fd183ea6ef0c9d87e43d6ce
Marie Malarme <malarmemarie@gmail.com>
2021-08-08 19:03:08 +0100

    mock data: define data structure on a data sample

---
+++ src/weeks_content.data.js
@@ -0,0 +1,122 @@
+export const weeks_content = [
+  {
+    id: 1,
+    dates: ['26•07•2021', '01•08•2021'],
+    nuggets: {
+      event: {
+        subtype: 'talk',
+        name: 'The mouse conference',
+        subtitle: 'What the mouse sees',
+        speakers: ['Daniel Shiffman', 'John Maeda', 'Casey Reas'],
+        description:
+          'The invention of the computer mouse helped to shift perceptions of the relationships between bodies and computing technologies. The mouse is powerful in its potential to reroute perceptions and create new meaning. The mouse can also be seen as a site of contradictions within the history of computing. These contradictions can be leveraged in order to complicate its history so that its story can be retold while it’s still here and still ubiquitous. Imagine a mouse re-imagined. What does it look like? Who is it for? How is it made? What materials? What does it feel like? Where can it be used? At this conference we will address a seemingly uninteresting and ubiquitous device in order to re-frame its relevance to and potential for living with and through computers. We have invited 8 artists and 6 speakers to present work through a complication of the computer mouse. The idea here is to re-imagine the mouse as an object. What in its shape and its material determine how a person uses it? How can thinking through the mouse help to re-evaluate our understanding of computing and the complex relationships between bodies and computers?',
+        image: '',
+        link: 'mouse-conference.com',
+      },
+      project: {
+        name: 'Typographic session July 23-25',
+        subtitle: 'Typographic collaborative collages',
+        authors: ['Robin Eberwein'],
+        description:
+          'Typographic collaborative collages. Typographic collaborative collages. Typographic collaborative collages. Typographic collaborative collages. Typographic collaborative collages. Typographic collaborative collages.',
+        image: '',
+        link: 'https://www.instagram.com/p/CRyHCAahzY5',
+      },
+      book: {
+        name: 'Torrent',
+        subtitle: 'Illustration fanzine',
+        authors: ['Sammy Stein', 'Kevin Bray', 'Marijpol', 'etc'],
+        description: 'Illustration fanzine',
+        publisher: 'Lagon Revue',
+        release: 'May 2021',
+        image: 'torrent.jpg',
+        link: 'mouse-conference.com',
+      },
+      quote: {
+        text: 'Talk is cheap, show me the code.',
+        author: 'Linus Torvalds',
+        date: '2009',
+      },
+    },
+  },
+  {
+    id: 2,
+    dates: ['02•08•2021', '08•08•2021'],
+    nuggets: {
+      event: {
+        subtype: 'talk',
+        name: 'Typographics 2021',
+        subtitle: 'Yearly typo conference',
+        speakers: ['Daniel Shiffman'],
+        description: 'Yearly typo conference',
+        image: '',
+        link: 'typographics-conference.com',
+      },
+      project: {
+        name: 'Not my Tempo',
+        subtitle: 'Music covers',
+        authors: ['Robin Eberwein'],
+        description: 'Music covers',
+        image: '',
+        link: 'https://www.instagram.com/p/CRyHCAahzY5',
+      },
+      book: {
+        name: 'Exploriso: Low-tech Fine Art',
+        subtitle: 'Risography as an artistic process',
+        authors: ['Sven Tillack'],
+        publisher: 'Spector Books',
+        release: 'Feb 2021',
+        description: 'Risography as an artistic process',
+        image: 'exploriso.jpg',
+        link: 'mouse-conference.com',
+      },
+      quote: {
+        text: 'Hello hello',
+        author: 'Zone 01 pole content',
+        date: '2018',
+      },
+    },
+  },
+  {
+    id: 3,
+    dates: ['15•07•2021', '06•08•2021'],
+    nuggets: {
+      event: {
+        subtype: 'exhibition',
+        name: 'Rapture',
+        subtitle: 'Solo show in Lisbon',
+        artists: ['Ai Weiwei'],
+        description: `The famous Chinese artist & activist Ai Weiwei, who recently took up residence in Portugal - far, far away from his native country, has installed his latest exhibition in the vast warehouse of the Cordoaria Nacional in Lisbon. This historic building, now holding the title of national monument, is located along the Tagus river & measures nearly 400 by 50 meters; it was in this factory that the ropes, cables, sails & flags for the renowned Portuguese ships were made. Founded in 1771 by the Portuguese Marine, it definitely ceased its activity in 1998 and now serves as an exhibition center.
+The large dimensions of the space are a perfect fit for Weiwei's monumental installations: those already familiar with the artist's iconic pieces will be pleased to re-encounter with the hundreds-bicycles of steel structure or the giant inflated black zodiac carrying immigrants. 
+All the favorite topics of Weiwei are present, mixed up with Chinese folklore, ancient Greek and Egyptian mythologies and even, for the most recent pieces, Portuguese craftsmanship: war, immigration crisis, demonstrations, mass production, media influence, censorship... And the variety of mediums chosen by Weiwei to exploit his beloved subjects is appreciable: sculpture, installation, painting, fresco, drawing, video, photography, diorama, design or ceramic are as many disciplines used to support a strong guiding thread.
+Throughout the numerous pieces presented in "Rapture", a highly interesting aspect of Weiwei's production emerges: the geopolitical themes addressed are often illustrated by delicate & fragile figurative objects, most of which being the result of fabulously executed handmade art using noble materials: porcelain, jade, crystal, bamboo, wood or marble all bring a sacred dimension to the artist's message.`,
+        image: '',
+        link: 'https://www.visitlisboa.com/fr/evenements/ai-weiwei-rapture',
+      },
+      project: {
+        name: 'Blanck',
+        subtitle: 'Studio Proto',
+        authors: ['Jean-Jacques'],
+        description: 'Studio Proto',
+        image: '',
+        link: 'https://www.instagram.com/p/CRyHCAahzY5',
+      },
+      book: {
+        name: 'Typography for Screen',
+        subtitle: 'Type in Motion',
+        authors: ['Wang Shaoqiang'],
+        publisher: 'Hoaki',
+        release: 'Sep 2020',
+        description:
+          'Typography for Screen features various creative typography projects for the digital screen, such as motion graphics, webpage design, and conceptual experiments and explores typographic application in visual and information hierarchies within the digital screen environment. Exclusive interviews with designers and studios integrate different knowledge and design philosophies constituting a true source of inspiration for designers and students.',
+        image: 'typography-for-screen.jpg',
+        link: 'https://www.behance.net/gallery/89510299/Typography-for-Screen-Type-in-Motion',
+      },
+      quote: {
+        text: 'Programming is about managing complexity.',
+        author: 'Georges Romero',
+        date: '2003',
+      },
+    },
+  },
+]
