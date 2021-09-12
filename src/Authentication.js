dee8b41ca5e2720b81343401813fdca856269209
Marie Malarme <malarmemarie@gmail.com>
2021-09-12 17:20:22 +0100

    style: put the authentication button upon the rest

--- src/Authentication.js
+++ src/Authentication.js
@@ -11,4 +11,4 @@ export const Authentication = ({ is_signed_in }) => {
 }
 
 const Button =
-  Component.c_pointer.fw500.shadow_a_s.fixed.t_40.ba.r25.wm_v_rl.flex.ai_center.text_upright.pt55.pb15.text_left.b_black.w40.b_rad20.uppercase.ls2.fs8.div()
+  Component.c_pointer.zi1.fw500.shadow_a_s.fixed.t_40.ba.r25.wm_v_rl.flex.ai_center.text_upright.pt55.pb15.text_left.b_black.w40.b_rad20.uppercase.ls2.fs8.div()
