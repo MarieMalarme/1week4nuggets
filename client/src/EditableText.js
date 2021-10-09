cf2574d8e69df0c705b9f51bb09299ffba1f5eb7
Marie Malarme <malarmemarie@gmail.com>
2021-10-09 19:22:28 +0100

    editable text: update modifications in the spreadsheet 2.5 seconds after the user stopped typing in the input

--- client/src/EditableText.js
+++ client/src/EditableText.js
@@ -9,7 +9,7 @@ export const EditableText = ({ initial_value, row, column, ...props }) => {
   const { set_is_editing, set_last_update } = functions
 
   const [wrapper_ref, set_wrapper_ref] = useState(null)
-  const [textarea_ref, set_textarea_ref] = useState(null)
+  const [text_ref, set_text_ref] = useState(null)
   const [text, set_text] = useState(null)
 
   const [scrollable, set_scrollable] = useState(false)
@@ -26,21 +26,33 @@ export const EditableText = ({ initial_value, row, column, ...props }) => {
   // set input's text value and text state to the initial value
   // when the data is fetched or updated from the spreadsheet
   useEffect(() => {
-    if (!textarea_ref) return
-    textarea_ref.innerText = initial_value || ''
+    if (!text_ref) return
+    text_ref.innerText = initial_value || ''
     set_text(initial_value || '')
-  }, [textarea_ref, initial_value, row])
+  }, [text_ref, row])
 
   // check if the "description" input has scrollable content
   useLayoutEffect(() => {
-    if (!textarea_ref || !wrapper_ref || column !== 'description') return
+    if (!text_ref || !wrapper_ref || column !== 'description') return
     const height = wrapper_ref.clientHeight
-    const scroll_height = textarea_ref.scrollHeight
+    const scroll_height = text_ref.scrollHeight
     set_scrollable(scroll_height > height)
-  }, [text, wrapper_ref, textarea_ref, column])
+  }, [text, wrapper_ref, text_ref, column])
 
   // get the styled input component corresponding to the type of item
   const Input = inputs_components[column]
+
+  const update_nugget = () =>
+    update_nugget_cell({
+      type,
+      week_id,
+      new_value: text_ref.innerText,
+      nuggets_sheet_coords,
+      set_last_update,
+      column,
+      row,
+      id,
+    })
 
   // if the user is not signed in, do not display the component when the text is empty
   // so the user without writing access can't see the placeholder with instructions to edit
@@ -72,37 +84,41 @@ export const EditableText = ({ initial_value, row, column, ...props }) => {
         <Input
           id={column}
           block={!text?.length}
-          elemRef={set_textarea_ref}
+          elemRef={set_text_ref}
           placeholder={`+  Add ${column}`}
           contentEditable={is_signed_in && is_selected && 'plaintext-only'}
           c_text={is_signed_in && is_selected && 'plaintext-only'}
-          onInput={() => set_text(textarea_ref.innerText)}
-          onFocus={() => set_is_editing(column)} // enter edit mode when focusing the textarea
-          onBlur={() => {
-            // to do: also update spreadsheet after editing stops
+          onInput={() => {
+            const { innerText } = text_ref
+            const whitespace_value = !innerText.trim().length
+            set_text(innerText)
 
             // check if the input value has changed
-            if (text !== initial_value) {
-              // if the value is only spaces, set the text to an empty string
-              if (!text.trim().length && (!initial_value || text.length)) {
-                textarea_ref.innerText = ''
-                set_text('')
-                // if the inital value was already empty, do not update the spreadsheet with an empty string
-                if (!initial_value || initial_value === '') return
-              }
+            if (innerText === initial_value) return
+            // if the value is only whitespaces, or already empty, do not update the spreadsheet with an empty string
+            if (whitespace_value && (!initial_value || innerText.length)) return
 
-              // update the spreadsheet with the new value
-              update_nugget_cell({
-                type,
-                week_id,
-                new_value: text,
-                nuggets_sheet_coords,
-                set_last_update,
-                column,
-                row,
-                id,
-              })
+            // update the spreadsheet with the new value 2 seconds after the user stopped typing
+            clearTimeout(timeout_id)
+            timeout_id = setTimeout(update_nugget, 2500)
+          }}
+          onFocus={() => set_is_editing(column)} // enter edit mode when focusing the textarea
+          onBlur={() => {
+            // check if the input value has changed
+            if (text === initial_value) return
+
+            // if the value is only whitespaces, set the text to an empty string
+            if (!text.trim().length && (!initial_value || text.length)) {
+              text_ref.innerText = ''
+              set_text('')
+              // if the inital value was already empty, do not update the spreadsheet with an empty string
+              if (!initial_value || initial_value === '') return
             }
+
+            // clear the timeout id in case the function in the onInput event was already triggered
+            clearTimeout(timeout_id)
+            // update the spreadsheet with the new value
+            update_nugget()
 
             // exit edit mode when clicking outside the input
             set_is_editing(false)
@@ -117,7 +133,7 @@ export const EditableText = ({ initial_value, row, column, ...props }) => {
             if (breaking_line || (!pressing_enter && !pressing_escape)) return
             // blur the input to trigger the onBlur event
             // that updates the spreadsheet and exits the edit mode
-            textarea_ref.blur()
+            text_ref.blur()
           }}
           {...style}
         />
@@ -152,5 +168,6 @@ const inputs_components = {
   link: Component.ws_pre_w.uppercase.ol_none.ls2.fs10.w100p.grey3.span(),
 }
 
+let timeout_id
 const initial_fully_scrolled = { top: true, bottom: false }
 const Wrapper = Component.w100p.div()
